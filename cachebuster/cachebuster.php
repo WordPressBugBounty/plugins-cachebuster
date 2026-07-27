af2f9f0e8d5d4fa48f35293d7d78539fcf265d23
Sean Murphy <sean@iamseanmurphy.com>
2026-07-27 15:13:31 +0000

    1.9

--- cachebuster/cachebuster.php
+++ cachebuster/cachebuster.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Cachebuster
 Description: Append a file's modification time to its URL and prevent stale caches.
-Version: 1.8
+Version: 1.9
 Author: Albert Peschar
 Author URI: https://kiboit.com
 License: GPLv2 or later
